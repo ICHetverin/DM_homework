748ef7391b0d6ff99bb628c1f2bb05c315ec1415
Ivan <ivan_chetverin@mail.ru>
2025-05-31 10:53:40 +0300

    add vizualization and cover it with tests

--- program/lib/ast.h
+++ program/lib/ast.h
@@ -11,12 +11,19 @@
 #include <nonterminal.h>
 #include <memory>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 class ASTNode {
 public:
     virtual ~ASTNode() = default;
     virtual bool equals(const ASTNode* other) const = 0;
     virtual void print(int indent = 0) const = 0;
+    virtual std::string getGraphVizLabel() const = 0;
+    virtual void generateGraphViz(std::ostream& out, int& counter, const std::string& parentId = "") const = 0;
+
+    static void saveAsGraphViz(const ASTNode* root, const std::string& filename);
 };
 
 class Leaf : public ASTNode {
@@ -25,6 +32,8 @@ public:
     Leaf(Token t);
     bool equals(const ASTNode* other) const override;
     void print(int indent) const override;
+    std::string getGraphVizLabel() const override;
+    void generateGraphViz(std::ostream& out, int& counter, const std::string& parentId = "") const override;
 };
 
 class InnerNode : public ASTNode {
@@ -32,16 +41,14 @@ class InnerNode : public ASTNode {
     std::vector<std::unique_ptr<ASTNode>> children;
 public:
     InnerNode(NonTerminal n);
-
     template<typename... Args>
     InnerNode(NonTerminal n, Args&&... args) : nt(n) {
         (children.emplace_back(std::forward<Args>(args)), ...);
     }
 
-    void addChild(std::unique_ptr<ASTNode> child) {
-        children.push_back(std::move(child));
-    }
-
+    void addChild(std::unique_ptr<ASTNode> child);
     bool equals(const ASTNode* other) const override;
     void print(int indent) const override;
+    std::string getGraphVizLabel() const override;
+    void generateGraphViz(std::ostream& out, int& counter, const std::string& parentId = "") const override;
 };
